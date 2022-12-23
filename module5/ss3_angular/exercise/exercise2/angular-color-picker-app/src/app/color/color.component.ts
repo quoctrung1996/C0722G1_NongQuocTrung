3d8890d93c7fddbb9e67f9b27c1c366ee99d8db8
quoctrung1996 <trungnoo196@gmail.com>
2022-12-23 11:56:09 +0700

    up

---
+++ module5/ss3_angular/exercise/exercise2/angular-color-picker-app/src/app/color/color.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css']
+})
+export class ColorComponent implements OnInit {
+  color1: boolean;
+  color2: boolean;
+  color3: boolean;
+  img1:string;
+  img2:string;
+  img3:string;
+
+  constructor() {
+  }
+
+  choiseColor(){
+
+    if (this.color1==true){
+      this.img1="red";
+    }
+    if (this.color2==true){
+      this.img2="blue";
+    }
+    if (this.color3==true){
+      this.img3="yellow";
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
